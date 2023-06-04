06910150131c1dde8462ddea4205401537b937eb
Hitori'D <96323989+Zalfa58@users.noreply.github.com>
2023-06-04 14:45:28 +0700

    p

--- tugasFlutter/flutter_application_1/lib/loginGooglePage.dart
+++ tugasFlutter/flutter_application_1/lib/loginGooglePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginGooglePage extends StatelessWidget {
   const LoginGooglePage({Key? key}) : super(key: key);
@@ -13,19 +14,19 @@ class LoginGooglePage extends StatelessWidget {
           alignment: Alignment.center,
           children: [
             Image.asset(
-              "assets/logo.jpg",
+              "assets/Logo1.jpg",
               fit: BoxFit.cover,
               height: double.infinity,
               width: double.infinity,
               alignment: Alignment.center,
             ),
             Positioned(
-              top: 40,
-              child: Text(
-                "Tabungan Siswa", 
-                style: 
-                  GoogleFonts.montserrat(color: Colors.black, fontSize: 30),
-            ))
+                top: 40,
+                child: Text(
+                  "Tabungan Siswa",
+                  style:
+                      GoogleFonts.montserrat(color: Colors.black, fontSize: 30),
+                ))
           ],
         ),
       ),
